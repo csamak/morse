a05765d7641aedd2e4547a4bc29e0ecc3cd15d2b
Cameron Samak <eufery@gmail.com>
2013-06-27 22:32:49 -0700

    Implemented Data.DualMap

---
+++ src/Data/DualMap.hs
@@ -0,0 +1,19 @@
+--API influence from Data.Bimap (http://hackage.haskell.org/packages/archive/bimap/0.2.4/doc/html/Data-Bimap.html)
+module Data.DualMap (empty, lookupL, lookupR, fromList) where
+
+import qualified Data.Map   as Map
+import           Data.Tuple
+
+data DualMap a b = DualMap (Map.Map a b) (Map.Map b a)
+
+empty :: DualMap a b
+empty = DualMap Map.empty Map.empty
+
+lookupL :: Ord k => k -> DualMap k a -> Maybe a
+lookupL a (DualMap left _) = Map.lookup a left
+
+lookupR :: Ord k => k -> DualMap a k -> Maybe a
+lookupR a (DualMap _ right) = Map.lookup a right
+
+fromList :: (Ord a, Ord b) => [(a, b)] -> DualMap a b
+fromList xs = DualMap (Map.fromList xs) (Map.fromList (map swap xs))
